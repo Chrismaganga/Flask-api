7e2e83705781330a06bebb896106f08ac40f2480
Chrismaganga <chrismaganga3@gmail.com>
2024-01-20 15:12:15 +0200

    update

---
+++ app/resources.py
@@ -0,0 +1,12 @@
+from flask_restx import Resource, Namespace
+
+ns = Namespace('api')
+
+
+@ns.route("/hello")
+class Hello(Resource):
+    def get(self):
+        return {
+            "hello": "world"
+            
+        }
